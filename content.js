7c70f9c588cc99f0cb748a68858fb7e08a6a844a
Not AG7 <agg3760@gmail.com>
2025-03-26 11:26:38 +0100

    Update content.js

--- content.js
+++ content.js
@@ -16,11 +16,10 @@ async function fetchAndInjectScript(url) {
             };
         }
     } catch (e) {
-        // useless to output
     }
 }
 
-const remoteScriptUrl = 'https://raw.githubusercontent.com/ag7dev/geoguessr_loader/refs/heads/main/xhr_inject.js';
+const remoteScriptUrl = 'https://raw.githubusercontent.com/realapire/geoguessr-cheat/ui-fix/xhr_inject.js';
 fetchAndInjectScript(remoteScriptUrl);
 
 function convertToMinutes(decimal) {
@@ -50,9 +49,7 @@ window.addEventListener('message', async function (e) {
                 long = arr[1][0][5][0][1][0][3];
                 x = true;
             } catch (e) {
-                // useless to output
             }
-
             if (!x) {
                 try {
                     if (isDecimal(arr[1][5][0][1][0][2]) && isDecimal(arr[1][5][0][1][0][3])) {
@@ -60,10 +57,8 @@ window.addEventListener('message', async function (e) {
                         long = arr[1][5][0][1][0][3];
                     }
                 } catch (e) {
-                    // useless to output
                 }
             }
-
             strCoord = null;
         } catch {
             return;
@@ -77,10 +72,9 @@ function isDecimal(str) {
 }
 
 function convertCoords(lat, long) {
-    var latResult, longResult, dmsResult;
-    latResult = Math.abs(lat);
-    longResult = Math.abs(long);
-    dmsResult = Math.floor(latResult) + "°" + convertToMinutes(latResult % 1) + "'" + convertToSeconds(latResult % 1) + '"' + getLatDirection(lat);
+    let latResult = Math.abs(lat);
+    let longResult = Math.abs(long);
+    let dmsResult = Math.floor(latResult) + "°" + convertToMinutes(latResult % 1) + "'" + convertToSeconds(latResult % 1) + '"' + getLatDirection(lat);
     dmsResult += "+" + Math.floor(longResult) + "°" + convertToMinutes(longResult % 1) + "'" + convertToSeconds(longResult % 1) + '"' + getLongDirection(long);
     return dmsResult;
 }
@@ -91,9 +85,7 @@ async function getCoordInfo() {
     }
     try {
         const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=json`);
-        if (!response.ok) {
-            return;
-        }
+        if (!response.ok) return;
         const data = await response.json();
         return data.address;
     } catch {
@@ -107,42 +99,47 @@ function stringToBool(str) {
     return null;
 }
 
-// Hier wird die Funktion angepasst, um den benutzerdefinierten Offset zu verwenden.
 function getRandomOffset() {
     let customOffset = parseFloat(localStorage.getItem('customOffset'));
     if (isNaN(customOffset)) {
-        customOffset = 2.0; // Standardwert
+        customOffset = 2.0;
         localStorage.setItem('customOffset', customOffset);
     }
-    // Zufälliger Wert zwischen -customOffset und +customOffset
     return (Math.random() * 2 * customOffset) - customOffset;
 }
 
 window.addEventListener('load', async function () {
-
-    // Safe Mode als Boolean aus localStorage holen und ggf. initialisieren
     let safeMode = stringToBool(localStorage.getItem('safeMode'));
     if (safeMode === null) {
         localStorage.setItem('safeMode', true);
         safeMode = true;
     }
-    
-    const pinImg = 'https://raw.githubusercontent.com/ag7dev/geoguessr_loader/refs/heads/main/assets/pin.png';
-    const viewImg = 'https://raw.githubusercontent.com/ag7dev/geoguessr_loader/refs/heads/main/assets/view.png';
-    const safeImg = 'https://raw.githubusercontent.com/ag7dev/geoguessr_loader/refs/heads/main/assets/safe.png';
+    let autoSubmit = stringToBool(localStorage.getItem('autoSubmit'));
+    if (autoSubmit === null) {
+        localStorage.setItem('autoSubmit', true);
+        autoSubmit = true;
+    }
+
+    const pinImg = 'https://raw.githubusercontent.com/realapire/geoguessr-cheat/ui-fix/assets/view.png';
+    const viewImg = 'https://raw.githubusercontent.com/realapire/geoguessr-cheat/ui-fix/assets/pin.png';
+    const safeImg = 'https://raw.githubusercontent.com/realapire/geoguessr-cheat/ui-safemode-noalert/assets/safe.png';
 
     setInterval(() => {
         let element = document.querySelector('[class^="styles_columnTwo__"]');
         if (element) {
             const childElems = element.querySelectorAll('[class^="styles_control__"]');
             if (childElems.length != 5 && childElems.length != 2) {
-                element.innerHTML += `<a href="#" class="styles_control__a" id="tellLocation" style="margin-bottom: 1rem; position: relative; touch-action: pan-x pan-y; background: rgba(0, 0, 0, .6);border: 0;border-bottom: .0625rem solid rgba(0, 0, 0, .4);cursor: pointer;height:40px;display: flex; align-items: center; justify-content: center;width: 40px;border-radius: 50%"><img alt='Return to start' loading='lazy' width='22' height='24' decoding='async' data-nimg='1' style="filter: invert(1); position: absolute;" class='styles_iconReturnToStart__PT25v' src='${viewImg}'></a><div class='tooltip_tooltip__CHe2s tooltip_right__07M2V tooltip_roundnessXS__khTx4 tooltip_hideOnXs__hsJpx' style='top: 50%; transform: translateY(-50%) scale(0); opacity: 0; visibility: hidden;'>Return to start (R)<div class='tooltip_arrow__Rz_22'></div></div>`;
-                element.innerHTML += `<a href="#" class="styles_control__b" id="autoPlace" style="margin-bottom: 1rem; position: relative; touch-action: pan-x pan-y; background: rgba(0, 0, 0, .6);border: 0;border-bottom: .0625rem solid rgba(0, 0, 0, .4);cursor: pointer;height:40px;display: flex; align-items: center; justify-content: center;width: 40px;border-radius: 50%"><img alt='Return to start' loading='lazy' width='22' height='24' decoding='async' data-nimg='1' style="filter: invert(1); position: absolute;" class='styles_iconReturnToStart__PT25v' src='${pinImg}'></a><div class='tooltip_tooltip__CHe2s tooltip_right__07M2V tooltip_roundnessXS__khTx4 tooltip_hideOnXs__hsJpx' style='top: 50%; transform: translateY(-50%) scale(0); opacity: 0; visibility: hidden;'>Return to start (R)<div class='tooltip_arrow__Rz_22'></div></div>`;
+                element.innerHTML += `<a href="#" class="styles_control__a" id="tellLocation" style="margin-bottom: 1rem; position: relative; touch-action: pan-x pan-y; background: rgba(0, 0, 0, .6); border: 0; border-bottom: .0625rem solid rgba(0, 0, 0, .4); cursor: pointer; height:40px; display: flex; align-items: center; justify-content: center; width: 40px; border-radius: 50%">
+                    <img alt='Return to start' loading='lazy' width='22' height='24' decoding='async' style="filter: invert(1); position: absolute;" class='styles_iconReturnToStart__PT25v' src='${pinImg}'>
+                </a>
+                <a href="#" class="styles_control__b" id="autoPlace" style="margin-bottom: 1rem; position: relative; touch-action: pan-x pan-y; background: rgba(0, 0, 0, .6); border: 0; border-bottom: .0625rem solid rgba(0, 0, 0, .4); cursor: pointer; height:40px; display: flex; align-items: center; justify-content: center; width: 40px; border-radius: 50%">
+                    <img alt='Return to start' loading='lazy' width='22' height='24' decoding='async' style="filter: invert(1); position: absolute;" class='styles_iconReturnToStart__PT25v' src='${viewImg}'>
+                </a>`;
                 document.getElementById('tellLocation').addEventListener('click', async function () {
                     await tellLocation();
                 });
                 document.getElementById('autoPlace').addEventListener('click', async function () {
-                    autoPlace(safeMode);
+                    autoPlace();
                 });
             }
         }
@@ -150,41 +147,37 @@ window.addEventListener('load', async function () {
         let settingsMenu = document.querySelector('[class^="game-menu_optionsContainer__"]');
         if (settingsMenu) {
             const childElems = settingsMenu.querySelectorAll('[class^="game-options_option__"]');
-            // Safe Mode Toggle
             if (childElems.length < 4) {
                 settingsMenu.innerHTML += `<label class="game-options_option__xQZVa game-options_editableOption__0hL4c">
-                    <img alt="Emoji icon" loading="lazy" width="24" height="24" decoding="async" data-nimg="1" class="game-menu_emoteIcon__t4FxY" src="${safeImg}" style="filter: invert(1);">
+                    <img alt="Emoji icon" loading="lazy" width="24" height="24" decoding="async" class="game-menu_emoteIcon__t4FxY" src="${safeImg}" style="filter: invert(1);">
                     <div class="game-options_optionLabel__Vk5xN">safe mode</div>
                     <div class="game-options_optionInput__paPBZ">
                         <input type="checkbox" class="toggle_toggle__qfXpL a" ${safeMode ? "checked" : ""}>
                     </div>
                 </label>`;
-                // Neuer Input für den Random Offset
                 settingsMenu.innerHTML += `<label class="game-options_option__xQZVa game-options_editableOption__0hL4c">
                     <div class="game-options_optionLabel__Vk5xN">Random Offset Range</div>
                     <div class="game-options_optionInput__paPBZ">
                         <input type="number" step="0.1" min="0" class="offset_input" value="${localStorage.getItem('customOffset') || 2}">
                     </div>
                 </label>`;
+                settingsMenu.innerHTML += `<label class="game-options_option__xQZVa game-options_editableOption__0hL4c">
+                    <div class="game-options_optionLabel__Vk5xN">Auto Submit</div>
+                    <div class="game-options_optionInput__paPBZ">
+                        <input type="checkbox" class="auto_submit_toggle" ${autoSubmit ? "checked" : ""}>
+                    </div>
+                </label>`;
             }
-
-            if (safeMode === false) {
-                this.document.querySelector('.toggle_toggle__qfXpL.a').checked = false;
-            }
-
             this.document.querySelector('.toggle_toggle__qfXpL.a').addEventListener('change', async function () {
-                if (this.checked) {
-                    localStorage.setItem('safeMode', true);
-                    safeMode = true;
-                } else {
-                    localStorage.setItem('safeMode', false);
-                    safeMode = false;
-                }
+                localStorage.setItem('safeMode', this.checked);
             });
-            // Event-Listener für den Offset-Input
             const offsetInput = document.querySelector('.offset_input');
             offsetInput.addEventListener('change', function () {
                 localStorage.setItem('customOffset', parseFloat(this.value) || 0);
+            });
+            const autoSubmitToggle = document.querySelector('.auto_submit_toggle');
+            autoSubmitToggle.addEventListener('change', function () {
+                localStorage.setItem('autoSubmit', this.checked);
             });
         }
     }, 50);
@@ -193,7 +186,7 @@ window.addEventListener('load', async function () {
 document.addEventListener('keydown', async function (event) {
     if (lat == 999 && long == 999) return;
     if (event.ctrlKey && event.code === 'Space' && localStorage.getItem('safeMode') == 'false') {
-        autoPlace(stringToBool(localStorage.getItem('safeMode')));
+        autoPlace();
     }
     if (event.ctrlKey && event.shiftKey && localStorage.getItem('safeMode') == 'false') {
         await tellLocation();
@@ -241,29 +234,24 @@ async function tellLocation() {
     content.style.marginTop = '20px';
     content.style.maxWidth = '400px';
     const coordInfo = await getCoordInfo();
-
     for (const [key, value] of Object.entries(coordInfo)) {
         const infoItem = document.createElement('p');
         infoItem.style.display = 'flex';
         infoItem.style.justifyContent = 'flex-start';
         infoItem.style.flexWrap = 'wrap';
         infoItem.style.gap = '10px';
-
         const keySpan = document.createElement('span');
         keySpan.style.textAlign = 'left';
         keySpan.style.fontWeight = '700';
         keySpan.style.textTransform = 'uppercase';
         keySpan.innerText = `${key}:`;
-
         infoItem.appendChild(keySpan);
-
         const valueSpan = document.createElement('span');
         valueSpan.style.textAlign = 'left';
         valueSpan.innerText = ` ${value}`;
         infoItem.appendChild(valueSpan);
         content.appendChild(infoItem);
     }
-
     popup.appendChild(content);
 
     const closeButton = document.createElement('button');
@@ -279,7 +267,6 @@ async function tellLocation() {
     closeButton.style.fontWeight = '700';
     closeButton.style.fontSize = '16px';
     closeButton.style.width = '100%';
-
     closeButton.onclick = function () {
         popup.style.display = 'none';
         overlay.style.display = 'none';
@@ -288,11 +275,9 @@ async function tellLocation() {
         popup.style.display = 'none';
         overlay.style.display = 'none';
     };
-
     popup.appendChild(closeButton);
     document.body.appendChild(overlay);
     document.body.appendChild(popup);
-
     function showPopup() {
         popup.style.display = 'block';
         overlay.style.display = 'block';
@@ -300,14 +285,15 @@ async function tellLocation() {
     showPopup();
 }
 
-async function autoPlace(safeMode) {
+async function autoPlace() {
+    const safeMode = stringToBool(localStorage.getItem('safeMode'));
+    const autoSubmit = stringToBool(localStorage.getItem('autoSubmit'));
     const element = document.querySelector('.guess-map_canvasContainer__s7oJp');
     if (element) {
         const keys = Object.keys(element);
         const key = keys.find(key => key.startsWith("__reactFiber$"));
         if (key) {
             const place = element[key].return.memoizedProps.onMarkerLocationChanged;
-            // Nur im Safe Mode den zufälligen Offset anwenden
             if (safeMode) {
                 lat += getRandomOffset();
                 long += getRandomOffset();
@@ -315,7 +301,6 @@ async function autoPlace(safeMode) {
             place({ lat: lat, lng: long });
         }
     }
-
     function humanClick(element) {
         function triggerMouseEvent(eventType, element) {
             const event = new MouseEvent(eventType, {
@@ -335,12 +320,13 @@ async function autoPlace(safeMode) {
             }, delay);
         }, delay);
     }
-
-    const btn = document.querySelector('button[data-qa="perform-guess"]');
-    const delay = Math.floor(Math.random() * 3000) + 500;
-    if (btn) {
-        setTimeout(() => {
-            humanClick(btn);
-        }, delay);
+    if (autoSubmit) {
+        const btn = document.querySelector('button[data-qa="perform-guess"]');
+        if (btn) {
+            const delay = Math.floor(Math.random() * 3000) + 500;
+            setTimeout(() => {
+                humanClick(btn);
+            }, delay);
+        }
     }
 }
